5e726dbcbc8b3719947df572af0d77791c74c634
latin bubel <esterapietkiewicz0110@gmail.com>
2024-04-13 17:16:43 +0200

    Added buttons to section 4 + minor changes in the newsletter section
    renamed js file, created a container for buttons, in js file:
    - created buttons text and colors arrays
    - added button creation when DOMContentLoaded event
    - added event listener to execute code when DOM content is loaded
    - created button by assigning arrays to buttons using forEach
    
    + minor changes in newsletter section : added id to the html and therefore changed up the css

---
+++ src/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonTexts = [
+    "Engaging games",
+    "Quizzes",
+    "Pre-recorded videos",
+    "Discussion forums",
+    "Hands-on activities",
+    "Assessments",
+  ];
+  const colors = [
+    "#B93E00",
+    "#1E287D",
+    "#1D62D9",
+    "#CE0053",
+    "#007749",
+    "#951CF2",
+  ];
+
+  const buttonContainer = document.getElementById("buttonContainer");
+
+  buttonTexts.forEach((text, index) => {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.style.backgroundColor = colors[index];
+    buttonContainer.appendChild(button);
+  });
+});
