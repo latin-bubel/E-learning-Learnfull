2bbd28e9089baf3d01fb39dafa79b3b2ebc4aa3d
latin bubel <esterapietkiewicz0110@gmail.com>
2024-04-14 12:14:14 +0200

    created object of  question, answer options and correct answer

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// index.html
 document.addEventListener("DOMContentLoaded", function () {
   const buttonTexts = [
     "Engaging games",
@@ -26,7 +27,37 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// form.html
+const questions = [
+  {
+    question: "What is 5 + 3?",
+    options: ["6", "7", "8", "9"],
+    answer: "8",
+  },
+  {
+    question: "What is 10 - 2?",
+    options: ["6", "7", "8", "9"],
+    answer: "8",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Mercury", "Venus", "Earth", "Jupiter"],
+    answer: "Jupiter",
+  },
+  {
+    question: "What is 4 * 3?",
+    options: ["10", "11", "12", "13"],
+    answer: "12",
+  },
+  {
+    question: "What is 15 / 3?",
+    options: ["3", "4", "5", "6"],
+    answer: "5",
+  },
+];
+
 // Function to handle click on answer form and change style property when chosen
+
 function selectOption(selectedOption) {
   var allOptions = document.querySelectorAll(".anwerForm");
   allOptions.forEach(function (option) {
